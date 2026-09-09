1646a573d424c71dbe23e9f97fb8e393938bf6a9
Artem <artemlarin931@gmail.com>
2026-09-10 00:59:38 +0300

    Добавлена реализация класса Треугольник

--- exampleMVS.cpp/exampleMVS.cpp.cpp
+++ exampleMVS.cpp/exampleMVS.cpp.cpp
@@ -1,12 +1,37 @@
 ﻿#include <iostream>
 
+class Triangle {
+	double _side;
+	double _height;
+public:
+	Triangle(double side, double height) {
+		_side = side;
+		_height = height;
+	}
+	double calculate_S() const {
+		return 0.5 * _side * _height;
+	}
+	void set_side(double side) {
+		_side = side;
+	}
+	void set_height(double height) {
+		_height = height;
+	}
+	double get_side() const {
+		return _side;
+	}
+	double get_height() const {
+		return _height;
+	}
+};
+
 int main() {
-    std::cout << "Input side and height of triangle:  " << std::endl;
-    double side;
-    double h;
-    std::cin >> side;
-    std::cin >> h;
-    double S = 0.5 * side * h;
-    std::cout << S;
-    return 0;
+	std::cout << "Input side and height of triangle:  " << std::endl;
+	double side, height;
+	std::cin >> side;
+	std::cin >> height;
+	Triangle T(side, height);
+	double S = T.calculate_S();
+	std::cout << S;
+	return 0;
 }
