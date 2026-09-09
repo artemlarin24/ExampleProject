2fea5fe989c16314b99ad233b6634db11ac3d3c7
Artem <artemlarin931@gmail.com>
2026-09-10 00:48:59 +0300

    Изменён файл ExampleMVS.cpp MAIN ВЫВОДИТ В КОНСОЛЬ ПЛОЩАДЬ ТРЕУГОЛЬНИКА ПО ЗАДАНИЮ ПАРАМЕТРАМ

--- exampleMVS.cpp/exampleMVS.cpp.cpp
+++ exampleMVS.cpp/exampleMVS.cpp.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
 
 int main() {
-    std::cout << "Hello" << std::endl;
+    std::cout << "Input side and height of triangle:  " << std::endl;
+    double side;
+    double h;
+    std::cin >> side;
+    std::cin >> h;
+    double S = 0.5 * side * h;
+    std::cout << S;
     return 0;
 }
